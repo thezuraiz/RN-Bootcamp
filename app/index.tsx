ca66769320f5305091044a50b59ff5ce43000614
The Zuraiz <thezuraiz@gmail.com>
2025-08-07 03:42:46 +0500

    Setup React Natiive Development enviroment

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#2b2b2bff",
+      }}
+    >
+      <Text style={{ color: "#ffffff" }}>Hello Zuraiz</Text>
+    </View>
+  );
+}
