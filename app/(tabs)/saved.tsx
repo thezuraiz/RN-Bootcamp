755f3029d807b614728229e72b6dd67665138788
The Zuraiz <thezuraiz@gmail.com>
2025-08-18 14:45:20 +0500

    Add Profile and Save Screen

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="flex-1 px-10 bg-primary">
+      <View className="flex flex-col items-center justify-center flex-1 gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-base text-gray-500">saved</Text>
+      </View>
     </View>
   );
 };
